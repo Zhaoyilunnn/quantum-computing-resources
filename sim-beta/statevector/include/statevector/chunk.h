752e1450909fbfb23a9cf126a656d2ba57402e29
Zhaoyilunnn <dilved1014@gmail.com>
2022-09-04 15:49:04 +0000

    Add todo

--- sim-beta/statevector/include/statevector/chunk.h
+++ sim-beta/statevector/include/statevector/chunk.h
@@ -5,6 +5,13 @@
 
 namespace sv {
 
+// TODO 
+// 1. Add data representing vector
+// 2. Add a manager to manager to actually perform data movement
+//    This manager should be configurable use template? 
+//    When storing current chunk to secondary storage,
+//    need info from next cluster
+// 3. Add chunk allocation   
 class Chunk {
 public:
     uint_t chunk_size_ = 0;
@@ -25,11 +32,6 @@ public:
     void apply_mcy(const reg_t& qubits);
 
     void apply_mcu(const reg_t& qubits, const cvector_t& mat);
-
-    // TODO `Add` data representing vector
-    //       Add a manager to manager to actually perform 
-    //       data movement
-    //       This manager should be configurable use template? 
 
 
 };
