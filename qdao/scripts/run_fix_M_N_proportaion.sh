61965375a0968fdba41bf5c45040afb7b1b7a6e9
Zhaoyilunnn <dilved1014@gmail.com>
2023-04-27 02:15:10 +0000

    qdao:: refine M-N exp scripts

--- qdao/scripts/run_fix_M_N_proportaion.sh
+++ qdao/scripts/run_fix_M_N_proportaion.sh
@@ -1,10 +1,29 @@
-res_dir=logs/qdao/shuguang/parallel/fix_M_to_N/$(date +%s)
+# Usage: ./$0 $mode $version
+# mode = parallel or no_parallel
+# version = quafu or qiskit
+
+if [ $# -eq 2 ]; then
+    mode=$1
+    version=$2
+else
+    mode=parallel
+    version=quafu
+fi
+
+
+res_dir=logs/qdao/shuguang/${mode}/fix_M_to_N/${version}/$(date +%s)
 mkdir $res_dir
 
 for i in $(seq 22 32); do
-    pytest -s -k test_run_quafu_random_basic tst/qdao/engine.py \
+    np=$((i-2))
+    if [ $i -le 29 ]; then
+        nl=$((i-6))
+    else
+        nl=24
+    fi
+    pytest -s -k test_run_${version}_random_basic tst/qdao/engine.py \
         --nq ${i} \
-        --np $((i-2)) \
-        --nl $((i-6)) |
-        tee ${res_dir}/rqc.${i}.qasm.log
+        --np ${np} \
+        --nl ${nl} |
+        tee ${res_dir}/rqc.${i}-${np}-${nl}.qasm.log
 done
