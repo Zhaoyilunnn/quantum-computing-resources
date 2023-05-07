292148b21390f19cc0c5615fd3a20d0a37bb53b5
Zhaoyilunnn <dilved1014@gmail.com>
2023-05-07 15:38:16 +0000

    qdao:: add L vs G script

--- tst/qdao/circuit.py
+++ tst/qdao/circuit.py
@@ -1,3 +1,5 @@
+from qiskit.circuit import QuantumCircuit
+from constants import QDAO_QASM_DIR
 from qdao.circuit import BasePartitioner, BaselinePartitioner, StaticPartitioner
 from qdao.test import QdaoBaseTest
 
@@ -45,6 +47,26 @@ class TestStaticPartitioner(QdaoBaseTest):
                 num_qubits=8, depth=20, measure=False)
         print(circ)
 
+        sub_circs = self._part.run(circ)
+        print("Number of sub-circuits: {}".format(len(sub_circs)))
+        print("Number operations in original circuit: {}".format(len(circ)))
+
+        sum_ops_sub_circs = sum([len(s.circ) for s in sub_circs])
+        assert sum_ops_sub_circs == len(circ) + len(sub_circs)
+
+    def test_run_any_qasm(self, nq, np, nl, qasm):
+        """Run for given qasm"""
+
+        NQ = int(nq)
+        NP = int(np)
+        NL = int(nl)
+
+        try:
+            circ = QuantumCircuit.from_qasm_file(QDAO_QASM_DIR + qasm)
+        except Exception as e:
+            print(f"Load from qasm file failed for {qasm}, error: {e}")
+            return
+        self._part = StaticPartitioner(np=NP, nl=NL)
         sub_circs = self._part.run(circ)
         print("Number of sub-circuits: {}".format(len(sub_circs)))
         print("Number operations in original circuit: {}".format(len(circ)))
