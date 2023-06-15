42a94c2506cc4ef769676f2d3f97f26a19c789e7
Zhaoyilunnn <dilved1014@gmail.com>
2023-06-15 09:54:57 +0000

    qvm:: test quafu backend trans helper

--- tst/misc/test_reorder.py
+++ tst/misc/test_reorder.py
@@ -2,13 +2,44 @@ from reorder import Reorder
 from bench import get_op_list_without_measure, get_op_lists
 import json
 
-test_input = [{"name": "u2", "params": [1.8661487719578789, 0.9139730132963093], "qubits": [2]}, {"name": "ccx", "qubits": [0, 3, 1]}, {"name": "ccx", "qubits": [3, 1, 0]}, {"name": "cswap", "qubits": [0, 3, 2]}, {"name": "rz", "params": [3.7076558050008965], "qubits": [0]}, {"name": "u1", "params": [-2.3561944901923457], "qubits": [1]}, {"name": "cu1", "params": [3.3698812961910276], "qubits": [2, 3]}, {"name": "u3", "params": [2.2241824136041903, 1.9155819994517396, 1.9208439411554306], "qubits": [2]}, {"name": "cswap", "qubits": [3, 0, 1]}]
-test_output = [[{"name": "u2", "params": [1.8661487719578789, 0.9139730132963093], "qubits": [2]}], [{"name": "ccx", "qubits": [0, 3, 1]}, {"name": "ccx", "qubits": [3, 1, 0]}], [{"name": "cswap", "qubits": [0, 3, 2]}, {"name": "rz", "params": [3.7076558050008965], "qubits": [0]}], [{"name": "u1", "params": [-2.3561944901923457], "qubits": [1]}, {"name": "cu1", "params": [3.3698812961910276], "qubits": [2, 3]}, {"name": "u3", "params": [2.2241824136041903, 1.9155819994517396, 1.9208439411554306], "qubits": [2]}], [{"name": "cswap", "qubits": [3, 0, 1]}]]
+test_input = [
+    {"name": "u2", "params": [1.8661487719578789, 0.9139730132963093], "qubits": [2]},
+    {"name": "ccx", "qubits": [0, 3, 1]},
+    {"name": "ccx", "qubits": [3, 1, 0]},
+    {"name": "cswap", "qubits": [0, 3, 2]},
+    {"name": "rz", "params": [3.7076558050008965], "qubits": [0]},
+    {"name": "u1", "params": [-2.3561944901923457], "qubits": [1]},
+    {"name": "cu1", "params": [3.3698812961910276], "qubits": [2, 3]},
+    {
+        "name": "u3",
+        "params": [2.2241824136041903, 1.9155819994517396, 1.9208439411554306],
+        "qubits": [2],
+    },
+    {"name": "cswap", "qubits": [3, 0, 1]},
+]
+test_output = [
+    [{"name": "u2", "params": [1.8661487719578789, 0.9139730132963093], "qubits": [2]}],
+    [{"name": "ccx", "qubits": [0, 3, 1]}, {"name": "ccx", "qubits": [3, 1, 0]}],
+    [
+        {"name": "cswap", "qubits": [0, 3, 2]},
+        {"name": "rz", "params": [3.7076558050008965], "qubits": [0]},
+    ],
+    [
+        {"name": "u1", "params": [-2.3561944901923457], "qubits": [1]},
+        {"name": "cu1", "params": [3.3698812961910276], "qubits": [2, 3]},
+        {
+            "name": "u3",
+            "params": [2.2241824136041903, 1.9155819994517396, 1.9208439411554306],
+            "qubits": [2],
+        },
+    ],
+    [{"name": "cswap", "qubits": [3, 0, 1]}],
+]
 
 CASE_STATIC_LOCAL = "test_data/archive/random.6.qobj.json"
 
-class TestReorder:
 
+class TestReorder:
     def test_run(self):
         reo = Reorder.get_reorder("static")
         reo.local_qubits = 3
@@ -29,10 +60,10 @@ class TestReorder:
         res = reo.result
         assert res[0]["instructions"][0]["qubits"] == [1]
         assert res[0]["instructions"][1]["qubits"] == [3]
-        assert res[0]["instructions"][2]["qubits"] == [3,1]
+        assert res[0]["instructions"][2]["qubits"] == [3, 1]
         assert res[0]["instructions"][3]["qubits"] == [1]
-        assert res[1]["instructions"][0]["qubits"] == [4,2]
+        assert res[1]["instructions"][0]["qubits"] == [4, 2]
         assert res[1]["instructions"][1]["qubits"] == [4]
-        assert res[2]["instructions"][0]["qubits"] == [0,5]
-        assert res[2]["instructions"][1]["qubits"] == [0,2]
+        assert res[2]["instructions"][0]["qubits"] == [0, 5]
+        assert res[2]["instructions"][1]["qubits"] == [0, 2]
         assert res[2]["instructions"][2]["qubits"] == [5]
--- tst/qvm/util/quafu_helper.py
+++ tst/qvm/util/quafu_helper.py
@@ -1,17 +1,22 @@
 from test.qvm import QvmBaseTest
 
-from qvm.util.quafu_helper import to_qiskit_backend_v1
+from qvm.util.quafu_helper import FakeCairo, to_qiskit_backend_v1
+from qiskit import transpile
 
 
 class TestQuafuHelper(QvmBaseTest):
-
     def test_to_qiskit_backend_v1(self):
         from quafu.users.userapi import User
-
 
         user = User()
         user.get_backends_info()
         backends = user.get_available_backends()
-        chip_info = backends['ScQ-P10'].get_chip_info()
+        chip_info = backends["ScQ-P10"].get_chip_info()
 
         backend = to_qiskit_backend_v1(chip_info["full_info"])
+
+        # Test compilation
+        circ = self.get_qiskit_circ("random", num_qubits=4, depth=3)
+        # trans_circ = transpile(circ, backend=FakeCairo())
+        trans_circ = transpile(circ, backend=backend)
+        print(trans_circ)
