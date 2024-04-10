00b8fb16cfd6cb4fc401099ee40330e1e4563110
Zhaoyilunnn <dilved1014@gmail.com>
2024-04-10 08:16:06 +0000

    feat: add quiet-s bv

--- learning/quafu/sample.py
+++ learning/quafu/sample.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np
-from quafu import QuantumCircuit, Task, User
+from quafu import QuantumCircuit, Task, User, simulate
 
 # user = User()
 ##user.save_apitoken("4DEH53vA_IxMSg_fyZ3grsJ4ew7Jn-itMS6bcPlJ7E9.Qf0UzM1QDNwgjNxojIwhXZiwSO2ITM6ICZpJye.9JiN1IzUIJiOicGbhJCLiQ1VKJiOiAXe0Jye")
@@ -21,13 +21,19 @@ if __name__ == "__main__":
     measures = [0, 1]
     cbits = [0, 1]
     q.measure(measures, cbits=cbits)
+    # q.unitary(np.array([[0, 1],[1, 0]]), [0])
+    q.draw_circuit()
+    res = simulate(q)
+    res.plot_probabilities()
 
-    task = Task()
-    task.config(backend="ScQ-P18", shots=2000, compile=True)
+    # task = Task()
+    # info = task.get_backend_info()
+    # task.config(backend="ScQ-P136", shots=2000, compile=True)
     # task.load_account()
+
 
     # res = task.send(q, wait=False, name="zhaoyilun-test", group=GROUP_NAME)
     # group_res = task.retrieve_group(GROUP_NAME)
     # print(group_res)
-    res = task.send(q, wait=True)
-    print(res)
+    # res = task.send(q, wait=True)
+    # print(res)
