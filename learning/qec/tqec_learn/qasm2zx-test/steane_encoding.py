f08f7b758e1651ff2eac3d644add5220e9d3e21b
Zhaoyilunnn <dilved1014@gmail.com>
2025-09-16 19:21:46 +0800

    chore: update steane code

--- learning/qec/tqec_learn/qasm2zx-test/steane_encoding.py
+++ learning/qec/tqec_learn/qasm2zx-test/steane_encoding.py
@@ -3,39 +3,34 @@ pyzx==0.9.0
 qiskit==2.1.1
 """
 
-from qiskit import QuantumCircuit
+from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
 import qiskit.qasm2 as qasm2
 from pyzx.circuit import Circuit as ZxCircuit
 
 
 def steane_encoding() -> QuantumCircuit:
-    qc = QuantumCircuit(8, 1, name="Steane Encoding")
+    qc = QuantumCircuit(10)
 
     qc.h(0)
-    qc.cx(0, 1)
-    qc.cx(0, 2)
     qc.cx(0, 3)
     qc.cx(0, 4)
-    qc.h(0)
-    qc.measure(0, 0)
-
-    qc.reset(0)
-    qc.h(0)
-    qc.cx(0, 1)
-    qc.cx(0, 2)
     qc.cx(0, 5)
     qc.cx(0, 6)
     qc.h(0)
-    qc.measure(0, 0)
 
-    qc.reset(0)
-    qc.h(0)
-    qc.cx(0, 1)
-    qc.cx(0, 3)
-    qc.cx(0, 5)
-    qc.cx(0, 7)
-    qc.h(0)
-    qc.measure(0, 0)
+    qc.h(1)
+    qc.cx(1, 3)
+    qc.cx(1, 4)
+    qc.cx(1, 7)
+    qc.cx(1, 8)
+    qc.h(1)
+
+    qc.h(2)
+    qc.cx(2, 3)
+    qc.cx(2, 5)
+    qc.cx(2, 7)
+    qc.cx(2, 9)
+    qc.h(2)
 
     return qc
 
@@ -49,7 +44,7 @@ def qiskit2zx(circuit: QuantumCircuit) -> ZxCircuit:
 if __name__ == "__main__":
     circuit = steane_encoding()
     print(circuit)
+    print(qasm2.dumps(circuit))
     zx_circuit = qiskit2zx(circuit)
+    zx_circuit.to_graph()
     print(zx_circuit)
-    zx_graph = zx_circuit.to_graph()
-    print(zx_graph)
